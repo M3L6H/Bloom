958ebdf678aae2e47c9c233e762424a9fa30d804
M3L6H <sisoac@hotmail.com>
2020-07-06 10:07:04 -0700

    Made session reducer respond to RECEIVE_CURRENT_USER

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,4 +1,7 @@
-import { RECEIVE_USER_LOGOUT } from '../actions/session_actions';
+import { 
+  RECEIVE_CURRENT_USER,
+  RECEIVE_USER_LOGOUT
+} from '../actions/session_actions';
 
 const _defaultState = {
   isAuthenticated: false,
@@ -9,6 +12,11 @@ export default (state = _defaultState, action) => {
   Object.freeze(state);
 
   switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return {
+        isAuthenticated: !!action.currentUser,
+        user: action.currentUser
+      };
     case RECEIVE_USER_LOGOUT:
       return _defaultState;
     default:
