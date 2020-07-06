ff5fe1c3a436235053acbfa30fd7c415673a07df
M3L6H <sisoac@hotmail.com>
2020-07-06 14:34:50 -0700

    Fixed passport configuration

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const { secretOrKey } = require("./keys");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
 const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -9,7 +11,14 @@ const options = {
 
 module.exports = passport => {
   passport.use(new JwtStrategy(options, (jwtPayload, done) => {
-    console.log(jwtPayload);
-    done();
+    User.findById(jwt_payload.id)
+      .then(user => {
+        if (user) {
+          return done(null, user);
+        }
+
+        return done(null, false);
+      })
+      .catch(err => console.log(err));
   }));
 };
