2ee3e97e1d74354fb560f4a5efc72b64bfcb234b
Ajay Rajamani <ajayrajamani@gmail.com>
2020-07-06 14:07:38 -0700

    Add NewHabit route and validations

--- routes/api/habits.js
+++ routes/api/habits.js
@@ -7,10 +7,12 @@
     const router = express.Router();
     const bcrypt = require('bcryptjs');
     const jwt = require('jsonwebtoken');
+    const passport = require("passport");
+
 /// Keys
     const keys = require('../../config/keys');
 /// Validations
-
+    const validateNewHabitInput = require("../../validation/new_habit");
 /// Models
     const Habit = require("../../models/Habit");
 
@@ -18,5 +20,25 @@
 
 router.get("/test",(req,res) => {res.json({msg:"habits"})});
 
+// Create Habit
+// expects req body to have keys title, description, and user(pojo with key "id")
+router.post("/", passport.authenticate("jwt", { session: false }), (req,res)=>{
+    
+    const {errors,isValid} = validateNewHabitInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    let newHabit = new Habit({
+        completed: false,
+        title: req.body.title ,
+        description: req.body.description || "",
+        user: req.body.user.id 
+    });
+
+    newHabit.save().then((habit)=> res.json(habit)); 
+
+});
 
 module.exports = router; 
