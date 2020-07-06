262ac415e13cebb50201bbf42c470d97b2077f1e
Ajay Rajamani <ajayrajamani@gmail.com>
2020-07-06 11:58:27 -0700

    Add users ref to Habit

--- models/Habit.js
+++ models/Habit.js
@@ -8,6 +8,11 @@ const Schema = mongoose.Schema;
 ////// Main
 
 const HabitSchema = new Schema({
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: "users"
+    },
+
     title: {
         type: String,
         required: true
@@ -20,6 +25,8 @@ const HabitSchema = new Schema({
         type: Boolean,
         required: true 
     }
+},{
+    timestamps:true
 })
 
 module.exports = Habit = mongoose.model("Habit", HabitSchema);
