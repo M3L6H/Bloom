d9ad902b87200e3ff1f1780897b673f4e49cceb7
M3L6H <sisoac@hotmail.com>
2020-07-07 12:07:27 -0700

    Checked ownership of habits

--- routes/api/habits.js
+++ routes/api/habits.js
@@ -9,8 +9,6 @@
     const jwt = require('jsonwebtoken');
     const passport = require("passport");
 
-/// Keys
-    const keys = require('../../config/keys');
 /// Validations
     const validateNewHabitInput = require("../../validation/new_habit");
 /// Models
@@ -24,80 +22,130 @@
 
 // Create Habit 
 // expects req body to have keys title, description,
-    router.post("/", passport.authenticate("jwt", { session: false }), (req,res)=>{
-        
-        const {errors,isValid} = validateNewHabitInput(req.body);
+router.post("/", passport.authenticate("jwt", { session: false }), (req,res)=>{
+  const {errors,isValid} = validateNewHabitInput(req.body);
 
-        if (!isValid) {
-            return res.status(400).json(errors);
-        }
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
-        let newHabit = new Habit({
-            completed: false,
-            title: req.body.title ,
-            description: req.body.description || "",
-            user: req.user.id 
-        });
+  let newHabit = new Habit({
+    completed: false,
+    title: req.body.title ,
+    description: req.body.description || "",
+    user: req.user.id 
+  });
 
-        newHabit.save().then((habit)=> res.json(habit)); 
-
-    });
+  newHabit.save().then((habit)=> res.json(habit)); 
+});
 
 //View Habit
-    router.get("/:id", passport.authenticate("jwt", { session: false }), (req,res)=>{
+router.get("/:id", passport.authenticate("jwt", { session: false }), async (req,res)=>{
+  let myHabit;
 
-        // Prevent the user from accessing another's habits!
-        if (req.user.id !== req.params.userId) {
-            return res.status(401).json("Unauthorized!");
-        } 
-
-        Habit.findById(req.params.id)
-            .then((habit)=> res.json(habit))
-            .catch(()=>res.json(null)); 
-    })
-
-// Delete Habit
-// Returns deleted habit id or "failed"
-    router.delete("/:id", passport.authenticate("jwt", { session: false }), (req,res) => {
-
-        // Prevent the user from accessing another's habits!
-        if (req.user.id !== req.params.userId) {
-            return res.status(401).json("Unauthorized!");
-        } 
-
-
-        Habit.deleteOne({_id: req.params.id})
-            .then((msg) => {
-                if(msg && msg.deletedCount > 0) {
-                    res.json({id: req.params.id});
-                } else {
-                    res.json("Failed");
-                }
-            })
-    })
-
-// Update Habit
-// Returns JSON of updated Habit or old habit if no updates are passed in body
-// Expects body to contain ONLY new key:value pairs
-    router.patch("/:id", passport.authenticate("jwt", { session: false }),(req,res)=>{
-        // Prevent the user from accessing another's habits!
-        if (req.user.id !== req.params.userId) {
-            return res.status(401).json("Unauthorized!");
-        } 
-
-        Habit.findOneAndUpdate({_id:req.params.id}, req.body,{new:true, lean:true})
-            .then(obj=>res.json(obj)); 
-    })
-
-router.post("/:id/tasks", passport.authenticate("jwt", { session: false }), async (req, res) => {
-  const myHabit = await Habit.findOne({ _id: req.params.id });
-
+  try {
+    myHabit = await Habit.findOne({ _id: req.params.id });
+  } catch(err) {
+    return res.status(422).json({ ...err, message: "Bad request." });
+  }
+  
+  // Check the habit exists
   if (!myHabit) {
     return res.status(404).json(`Could not find habit with id ${ req.params.id }`);
   }
 
+  // Check that we are the author
   if (req.user.id != myHabit.user) {
-    return res.status(401).json("Cannot edit another user's habits!");
+    return res.status(403).json("Cannot get another user's habits!");
+  }
+
+  res.json(myHabit);
+});
+
+// Delete Habit
+// Returns deleted habit id or "failed"
+router.delete("/:id", passport.authenticate("jwt", { session: false }), async (req,res) => {
+  let myHabit;
+
+  try {
+    myHabit = await Habit.findOne({ _id: req.params.id });
+  } catch(err) {
+    return res.status(422).json({ ...err, message: "Bad request." });
+  }
+  
+  // Check the habit exists
+  if (!myHabit) {
+    return res.status(404).json(`Could not find habit with id ${ req.params.id }`);
+  }
+
+  // Check that we are the author
+  if (req.user.id != myHabit.user) {
+    return res.status(403).json("Cannot delete another user's habits!");
+  }
+
+  Habit.deleteOne({_id: req.params.id})
+    .then((msg) => {
+      if(msg && msg.deletedCount > 0) {
+        res.json({id: req.params.id});
+      } else {
+        res.json("Failed");
+      }
+    })
+});
+
+// Update Habit
+// Returns JSON of updated Habit or old habit if no updates are passed in body
+// Expects body to contain ONLY new key:value pairs
+router.patch("/:id", passport.authenticate("jwt", { session: false }), async (req,res)=>{
+  let myHabit;
+
+  try {
+    myHabit = await Habit.findOne({ _id: req.params.id });
+  } catch(err) {
+    return res.status(422).json({ ...err, message: "Bad request." });
+  }
+  
+  // Check the habit exists
+  if (!myHabit) {
+    return res.status(404).json(`Could not find habit with id ${ req.params.id }`);
+  }
+
+  // Check that we are the author
+  if (req.user.id != myHabit.user) {
+    return res.status(403).json("Cannot edit another user's habits!");
+  }
+  
+  const {
+    title,
+    description,
+    completed
+  } = req.body;
+
+  myHabit.title = title || myHabit.title;
+  myHabit.description = description || myHabit.description;
+  myHabit.completed = completed || myHabit.completed;
+
+  myHabit.save()
+    .then(obj=>res.json(obj)); 
+});
+
+router.post("/:id/tasks", passport.authenticate("jwt", { session: false }), async (req, res) => {
+  let myHabit;
+
+  try {
+    myHabit = await Habit.findOne({ _id: req.params.id });
+  } catch(err) {
+    return res.status(422).json({ ...err, message: "Bad request." });
+  }
+
+  // Check that we are the author
+  if (!myHabit) {
+    return res.status(404).json(`Could not find habit with id ${ req.params.id }`);
+  }
+  
+  // Prevent the user from accessing another's habits
+  if (req.user.id != myHabit.user) {
+    return res.status(403).json("Cannot edit another user's habits!");
   }
 
   const { 
