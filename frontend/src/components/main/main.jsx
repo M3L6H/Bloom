58cd85f2ee607b1f3a52e079fc9b8e75ef1c4137
M3L6H <sisoac@hotmail.com>
2020-07-07 10:23:08 -0700

    Fixed flickering

--- frontend/src/components/main/main.jsx
+++ frontend/src/components/main/main.jsx
@@ -1,21 +1,45 @@
 import React from 'react';
 
+import img01 from "../../images/a01.jpg";
+import img02 from "../../images/a02.jpg";
+import img03 from "../../images/a03.jpg";
+import img04 from "../../images/a04.jpg";
+import img05 from "../../images/a05.jpg";
+import img06 from "../../images/a06.jpg";
+import img07 from "../../images/a07.jpg";
+
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.hydrangea = React.createRef();
 
-    render(){
-        const { openModal } = this.props;
+    window.addEventListener("load", () => {
+      console.log("called");
+      this.hydrangea.current.classList.add("animated");
+    });
+  }
 
-        return(
-            <div className="hydrangea">
-                <img src="https://i.ibb.co/9vrwdSr/bloom.png" alt="hydrangea"/>
-                <div className="main-buttons">
-                    <button className="ui button" onClick={() => openModal('login')}>Log In</button>
-                    <button className="ui button" onClick={() => openModal('signup')}>Sign Up</button>
-                </div>
-            </div>
-        )
-    }
+  render(){
+    const { openModal } = this.props;
+
+    return(
+      <div className="hydrangea" ref={ this.hydrangea }>
+        <img src="https://i.ibb.co/9vrwdSr/bloom.png" alt="hydrangea"/>
+        <img src={ img01 } style={{ display: "none" }} />
+        <img src={ img02 } style={{ display: "none" }} />
+        <img src={ img03 } style={{ display: "none" }} />
+        <img src={ img04 } style={{ display: "none" }} />
+        <img src={ img05 } style={{ display: "none" }} />
+        <img src={ img06 } style={{ display: "none" }} />
+        <img src={ img07 } style={{ display: "none" }} />
+        <div className="main-buttons">
+          <button className="ui button" onClick={() => openModal('login')}>Log In</button>
+          <button className="ui button" onClick={() => openModal('signup')}>Sign Up</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Main;
