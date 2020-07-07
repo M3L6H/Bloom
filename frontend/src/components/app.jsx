699c197616e3c7b941cbcccee389615e90303da5
Ashe <ashe@nyu.edu>
2020-07-07 10:02:10 -0700

    working on login

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import { Header } from 'semantic-ui-react';
 import Main from './main';
 import Modal from './modal/modal'
 
--- frontend/src/components/main/main.jsx
+++ frontend/src/components/main/main.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
-
 
 class Main extends React.Component {
 
-    constructor(props){
-        super(props)
-    }
 
     render(){
         const { openModal } = this.props;
 
         return(
             <div className="hydrangea">
-                <img src="https://i.ibb.co/9vrwdSr/bloom.png"/>
+                <img src="https://i.ibb.co/9vrwdSr/bloom.png" alt="hydrangea"/>
                 <div className="main-buttons">
                     <button className="ui button" onClick={() => openModal('login')}>Log In</button>
                     <button className="ui button" onClick={() => openModal('signup')}>Sign Up</button>
