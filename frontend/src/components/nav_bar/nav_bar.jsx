21e977d2d771cdda7db45463061c04b668f4dec7
Anna Cho <hbnch310@gmail.com>
2020-07-06 20:05:45 -0700

    Added profile dropdown

--- frontend/src/components/nav_bar/nav_bar.jsx
+++ frontend/src/components/nav_bar/nav_bar.jsx
@@ -7,11 +7,21 @@ class NavBar extends React.Component {
     }
 
     render() {
+        const dropdown = (
+            <div class="menu" >
+            <div class="item">Profile</div>
+            <div class="item">ele1</div>
+            <div class="item">ele2</div>
+            <div class="item">ele3</div>
+            </div >
+        )
         return(
             <div className="ui tabular menu">
                 <a href='#' className="active item"> first ele </a>
                 <a href='#' className="item"> second ele </a>
-                <i class="fas fa-bars"></i>
+                <i class="fas fa-bars">
+                    {dropdown}
+                </i>
             </div>
         )
     }
