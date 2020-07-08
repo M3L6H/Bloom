1b206fe7c306e7557d3298fabdb93fba3912b3d1
Anna Cho <hbnch310@gmail.com>
2020-07-08 16:03:55 -0700

    Blah

--- frontend/src/components/session_forms/signup_form_container.js
+++ frontend/src/components/session_forms/signup_form_container.js
@@ -1,16 +1,13 @@
-import { connect } from 'react-redux';
-import SignupForm from './signup_form';
-import { signup } from '../../actions/session_actions';
-import { closeModal } from '../../actions/modal_actions';
+import { connect } from "react-redux";
+import SignupForm from "./signup_form";
+import { signup } from "../../actions/session_actions";
+import { closeModal } from "../../actions/modal_actions";
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        signup: (user) => dispatch(signup(user)),
-        closeModal: () => dispatch(closeModal())
-    };
+  return {
+    signup: (user) => dispatch(signup(user)),
+    closeModal: () => dispatch(closeModal()),
+  };
 };
 
-export default connect(
-    null,
-    mapDispatchToProps
-)(SignupForm)
+export default connect(null, mapDispatchToProps)(SignupForm);
