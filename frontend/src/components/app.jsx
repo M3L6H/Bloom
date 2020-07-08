495a46d2f47c3897f8825e5928025060a000d7d0
M3L6H <sisoac@hotmail.com>
2020-07-08 15:43:33 -0700

    Finished jar graphics

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -5,15 +5,12 @@ import Modal from './modal/modal';
 import CreateHabit from './habit';
 import AsheNavBar from './ashe_nav_bar';
 
-import Jar from './jar';
-
 const App = () => (
   <div className='test'>
-    <Jar />
-    {/* <Modal />
+    <Modal />
     <AuthRoute path="/" component={Main}/>
     <ProtectedRoute path="/" component={AsheNavBar} />
-    <ProtectedRoute path="/habit" component={CreateHabit}/> */}
+    <ProtectedRoute path="/habit" component={CreateHabit}/>
   </div>
 );
 
--- frontend/src/components/jar/jar.jsx
+++ frontend/src/components/jar/jar.jsx
@@ -2,53 +2,68 @@ import React, { Component } from 'react';
 import withWindowDimensions from '../hocs/with_window_dimensions';
 
 import p2 from 'p2';
+import petal from "../../images/petal.png";
 
 class Jar extends Component {
   constructor(props) {
     super(props);
   
     this.canvasRef = React.createRef();
+
+    // Setting up dimensions
+    this.windowPercentage = 0.5;
+
+    const { windowWidth, windowHeight } = this.props;
+
+    this.unit = Math.min(windowWidth, windowHeight * this.windowPercentage) / 75;
+    this.innerDiameter = 40 * this.unit;
+    this.topOffset = 15 * this.unit;
+    this.petalSize = 9;
+
+    console.log(this.unit);
+
+    // Set up world
     this.world = new p2.World({
-      gravity: [0, -9.82]
+      gravity: [0, 75 * this.unit]
     });
 
-    const polyBody = new p2.Body({
-      mass: 1,
-      position: [5, 10]
-    });
-    console.log(polyBody.fromPolygon([[0, 0], [0, 20], [30, 20], [30, 0], [20, 0], [20, 10], [10, 10], [10, 0]]));
-    
-    this.world.addBody(polyBody);
-
-    const circleBody = new p2.Body({
-      mass: 5,
-      position: [0, 10]
-    });
-    circleBody.addShape(new p2.Circle({ radius: 1 }));
-
-    this.world.addBody(circleBody);
-
-    const groundBody = new p2.Body({
-      mass: 0,
-      position: [0, 30]
-    });
-    groundBody.addShape(new p2.Plane());
-
-    this.world.addBody(groundBody);
-
+    // Initializing state
     this.state = {
-      polyBody,
-      circleBody,
-      groundBody
+      lastTime: null
     };
 
+    // Simulation constants
     this.fixedTimeStep = 1 / 60;
     this.maxSubSteps = 10;
 
+    // Binding functions
     this._updateAnimation = this._updateAnimation.bind(this);
+
+    // Body id
+    this.bodyId = 1;
+
+    // Appearance
+    this.jarColor = "rgb(230, 230, 255)";
+
+    // Initial values
+    this.jar = null;
+    this.petals = [];
+
+    // Set up petals image
+    this.petalImgSize = 72 * this.unit / 7.3;
+    this.petalImg = new Image(this.petalImgSize, this.petalImgSize);
+    this.petalImg.src = petal;
   }
 
   componentDidMount() {
+    // Create jar
+    const { windowWidth } = this.props;
+    const position = [(windowWidth - this.innerDiameter) / 2 - 3 * this.unit, this.topOffset];
+    this.jar = this._createPolyBody(this._jarPath(), { position });
+
+    // Spawn petals
+    this._spawnPetals(10);
+    
     this.rAF = requestAnimationFrame(this._updateAnimation);
   }
 
@@ -56,11 +71,86 @@ class Jar extends Component {
     cancelAnimationFrame(this.rAF);
   }
 
+  _spawnPetals(amt) {
+    const { windowWidth } = this.props;
+    const petalPosition = [(windowWidth - this.innerDiameter) / 2 + 0.15 * this.innerDiameter + Math.random() * 0.7 * this.innerDiameter, this.topOffset + 5 * this.unit];
+    this.petals.push(this._createPolyBody(this._petalPath(), { position: petalPosition, mass: 1 }));
+
+    if (amt > 0) {
+      setTimeout(() => this._spawnPetals(amt - 1), 150);
+    }
+  }
+
+  _petalPath() {
+    return [
+      [0, 4 / 5 * this.petalSize * this.unit],
+      [this.petalSize * this.unit, 4 / 5 * this.petalSize * this.unit],
+      [3.5 / 5 * this.petalSize * this.unit, 0],
+      [1.5 / 5 * this.petalSize * this.unit, 0]
+    ];
+  }
+
+  _jarPath() {
+    const { windowHeight } = this.props;
+    const height = (windowHeight * this.windowPercentage) - (this.topOffset + this.unit);
+
+    return [
+      [2 * this.unit, 0],
+      [2 * this.unit, this.unit],
+      [this.unit, this.unit],
+      [this.unit, 2 * this.unit],
+      [2 * this.unit, 2 * this.unit],
+      [2 * this.unit, 4 * this.unit],
+      [this.unit, 4 * this.unit],
+      [this.unit, 5 * this.unit],
+      [2 * this.unit, 5 * this.unit],
+      [0, 8 * this.unit],
+      [0, height - 2 * this.unit],
+      [2 * this.unit, height],
+      [5 * this.unit + this.innerDiameter + 2 * this.unit, height],
+      [5 * this.unit + this.innerDiameter + 4 * this.unit, height - 2 * this.unit],
+      [5 * this.unit + this.innerDiameter + 4 * this.unit, 8 * this.unit],
+      [5 * this.unit + this.innerDiameter + 2 * this.unit, 5 * this.unit],
+      [5 * this.unit + this.innerDiameter + 3 * this.unit, 5 * this.unit],
+      [5 * this.unit + this.innerDiameter + 3 * this.unit, 4 * this.unit],
+      [5 * this.unit + this.innerDiameter + 2 * this.unit, 4 * this.unit],
+      [5 * this.unit + this.innerDiameter + 2 * this.unit, 2 * this.unit],
+      [5 * this.unit + this.innerDiameter + 3 * this.unit, 2 * this.unit],
+      [5 * this.unit + this.innerDiameter + 3 * this.unit, this.unit],
+      [5 * this.unit + this.innerDiameter + 2 * this.unit, this.unit],
+      [5 * this.unit + this.innerDiameter + 2 * this.unit, 0],
+      [5 * this.unit + this.innerDiameter, 0],
+      [5 * this.unit + this.innerDiameter, 5 * this.unit],
+      [5 * this.unit + this.innerDiameter + 2 * this.unit, 8 * this.unit],
+      [5 * this.unit + this.innerDiameter + 2 * this.unit, height - 2 * this.unit],
+      [2 * this.unit, height - 2 * this.unit],
+      [2 * this.unit, 8 * this.unit],
+      [4 * this.unit, 5 * this.unit],
+      [4 * this.unit, 0]
+    ];
+  }
+
+  _createPolyBody(path, options={}) {
+    const { position, mass, velocity } = options;
+    const polyBody = new p2.Body({
+      mass: mass || 0,
+      position: position || [0, 0],
+      velocity: velocity || [0, 0]
+    });
+
+    if (!polyBody.fromPolygon(path)) return false;
+
+    this.world.addBody(polyBody);
+    return polyBody;
+  }
+
   _renderConvexShape(shape, ctx) {
     const [x, y] = shape.body.position;
     const [shapeX, shapeY] = shape.position;
 
     ctx.save();
+    ctx.fillStyle = this.jarColor;
+    ctx.strokeStyle = this.jarColor;
     ctx.beginPath();
     ctx.translate(x + shapeX, y + shapeY);
     ctx.rotate(shape.body.angle);
@@ -70,36 +160,54 @@ class Jar extends Component {
       ctx.lineTo(nextX, nextY);
     });
     ctx.fill();
-    // ctx.stroke();
+    ctx.stroke();
     ctx.restore();
+  }
+
+  _renderBody(body, ctx) {
+    body.shapes.forEach(shape => {
+      switch(shape.type) {
+        case p2.Shape.CONVEX:
+          this._renderConvexShape(shape, ctx);
+      }
+    });
+  }
+
+  _renderPetal(body, ctx) {
+    const [x, y] = body.position;
+
+    ctx.save();
+    ctx.translate(x, y);
+    ctx.rotate(body.angle);
+    ctx.drawImage(this.petalImg, -this.petalImgSize / 2, -this.petalImgSize / 2, this.petalImgSize, this.petalImgSize);
+    ctx.restore();
+  }
+
+  _renderBodies(ctx) {
+    this.petals.forEach(body => this._renderPetal(body, ctx));
+    this._renderBody(this.jar, ctx);
   }
   
   _updateAnimation(time) {
+    const { lastTime } = this.state;
+    
     // Update world
-    const deltaTime = this.lastTime ? (time - this.lastTime) / 1000 : 0;
+    const deltaTime = lastTime ? (time - lastTime) / 1000 : 0;
     this.world.step(this.fixedTimeStep, deltaTime, this.maxSubSteps);
 
-    console.log(this.state.polyBody);
-    console.log(this.state.circleBody);
-    console.log(this.state.groundBody);
-    
+    // Render canvas
     const canvas = this.canvasRef.current;
     const ctx = canvas.getContext("2d");
     const width = canvas.width;
     const height = canvas.height;
 
-    ctx.fillStyle = "rgb(90, 20, 20, 0.3)";
+    ctx.clearRect(0, 0, width, height);
 
-    ctx.fillRect(20, 20, 150, 100);
-
-    // this._renderConvexShape(this.state.polyBody.shapes[0], ctx);
-
-    this.state.polyBody.shapes.forEach(shape => {
-      this._renderConvexShape(shape, ctx);
-    });
+    this._renderBodies(ctx);
+    // console.log(this.world.bodies);
     
-    this.lastTime = time;
-    // this.rAF = requestAnimationFrame(this._updateAnimation);
+    this.setState({ lastTime: time });
+    this.rAF = requestAnimationFrame(this._updateAnimation);
   }
   
   render() {
@@ -108,7 +216,7 @@ class Jar extends Component {
     return (
       <canvas
         width={ windowWidth }
-        height={ windowHeight * 0.5 }
+        height={ windowHeight * this.windowPercentage }
         className="jar"
         ref={ this.canvasRef }
       ></canvas>
