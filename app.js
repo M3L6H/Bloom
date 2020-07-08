2c29dad387fe92580af3e4c5d1fd79f195bea01d
Ajay Rajamani <ajayrajamani@gmail.com>
2020-07-07 20:22:39 -0700

    Prepare for push

--- app.js
+++ app.js
@@ -32,6 +32,13 @@ app.use("/api/tasks", tasks);
 app.use("/api/users", users);
 app.use("/api/habits", habits);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${ port }`));
