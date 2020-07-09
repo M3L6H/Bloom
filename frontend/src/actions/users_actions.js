fc07ed3dda2bf9689836df5ff04fcbf57956bbac
Ajay Rajamani <ajayrajamani@gmail.com>
2020-07-09 11:38:54 -0700

    Add user thunk action for updating Daily Task List

--- frontend/src/actions/users_actions.js
+++ frontend/src/actions/users_actions.js
@@ -1,5 +1,5 @@
 import * as APIUtil from '../util/users_util';
-
+import {receiveTaskErrors} from "./tasks_actions"
 export const RECEIVE_USER = "RECEIVE_USER";
 export const RECEIVE_USER_ERRORS = "RECEIVE_USER_ERRORS";
 
@@ -23,4 +23,10 @@ export const updatePetals = (petals) => dispatch => (
   APIUtil.updatePetals(petals)
     .then(res => dispatch(receiveUser(res.data)))
     .catch(err => dispatch(receiveUserErrors(err.response.data)))
+);
+
+export const updateDailyTaskList = (dailyTaskList) => dispatch => (
+  APIUtil.updateDailyTaskList(dailyTaskList)
+    .then(res => dispatch(receiveUser(res.data)))
+    .catch(err => dispatch(receiveTaskErrors(err.response.data)))
 );
