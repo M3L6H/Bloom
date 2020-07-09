a411be93f64e54b2913e6eb870de2c8e6b365d8f
Anna Cho <hbnch310@gmail.com>
2020-07-09 16:52:02 -0700

    Errors on nav-bar

--- frontend/src/components/nav_bar/nav_bar.jsx
+++ frontend/src/components/nav_bar/nav_bar.jsx
@@ -1,27 +1,42 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 class NavBar extends React.Component {
-    render() {
-       
-        return(
-            <div className="ui tabular menu">
-                <a href='/' className="active item"> first ele </a>
-                <a href='/' className="item"> second ele </a>
-                <div className='dropdown'>
-                <i className='fa fa-bars' aria-hidden='true'></i>
-                    <div className='dropdown-content'>
-                            <a href="/">Profile</a>
-                            <a href="/">One</a>
-                            <a href="/">logout</a>
-                    </div>
-                </div>
 
+  render() {
+    return (
+      <div className="nav-bar">
+        <div className="left">
+          <div className="nav-first">
+            <Link className="plain" to="/">
+              First
+            </Link>
+          </div>
+          <div className="answers">
+            <Link className="plain" to="/">
+              Second
+            </Link>
+          </div>
+        </div>
+
+        <div className="right">
+          <div className="hamburger-icn">
+            <i className="fa fa-bars" aria-hidden="true"></i>
+            <div className="dropdown-content">
+              <ul>
+                <li>
+                  <Link to={`/`}>
+                    <span>My Profile</span>
+                  </Link>
+                </li>
+                <li onClick={this.props.logout}>Logout</li>
+              </ul>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NavBar;
-
-//<li onClick={this.props.logout}>Logout<li>
