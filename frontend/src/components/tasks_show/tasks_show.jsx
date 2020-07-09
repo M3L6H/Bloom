9ed5982be7d7e35198d79785b8f5bec3360304dd
M3L6H <sisoac@hotmail.com>
2020-07-09 15:05:16 -0700

    Moved set state out of render

--- frontend/src/components/tasks_show/tasks_show.jsx
+++ frontend/src/components/tasks_show/tasks_show.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import TaskShow from './task_show';
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 class TasksShow extends React.Component {
     constructor(props){
         super(props);
         this.dragEnd = this.dragEnd.bind(this); 
-        this.state = {}; 
+        this.state = {
+          loaded: false,
+          taskOrder: null
+        }; 
     }
 
-    componentDidMount(){
-
+    async componentDidMount(){
+        this._isMounted = true;
+      
         // Fetch the current user and their habits/tasks
-        this.props.fetchHabits();
-        this.props.fetchUser();
-        
+        await this.props.fetchHabits();
+        const { user } = await this.props.fetchUser();
+        if (this._isMounted) {
+          this.setState({ loaded: true, taskOrder: Array.from(user.dailyTaskList) });
+        }
     }
+
+    componentWillUnmount() {
+      this._isMounted = false;
+    }
+    
 
     dragEnd(res) {
         let newTaskList = Array.from(this.props.user.dailyTaskList);
@@ -25,15 +36,10 @@ class TasksShow extends React.Component {
     }
 
     render(){
-        const { tasks, habits, user } = this.props;
+        const { tasks, habits } = this.props;
 
         //Check for relevent props before rendering
-        if(!this.props.habits || !this.props.user){
-            return null; 
-        }
-
-        if (!this.state.taskOrder) {
-            this.setState({ taskOrder: Array.from(user.dailyTaskList) });
+        if(!this.state.loaded){
             return null; 
         }
 
