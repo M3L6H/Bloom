e14ed10fc0bab2fe0f43238dd730d2e36db3ba12
Ajay Rajamani <ajayrajamani@gmail.com>
2020-07-10 00:30:38 -0700

    Add drag and drop to habits without updating database

--- frontend/src/components/habit/habit_index.jsx
+++ frontend/src/components/habit/habit_index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import HabitIndexItem from './habit_index_item';
-
+import { DragDropContext, Droppable} from "react-beautiful-dnd";
 class HabitIndex extends React.Component {
 
     componentDidMount() {
@@ -14,14 +14,29 @@ class HabitIndex extends React.Component {
         if (!habits || !user) return null;
         return (
           <div className="background-index-habit">
-            <div className="show-habits-header">
-              <h3>Your Habits</h3>
-            </div>
-            <div className="index-habit-container">
-              {user.habits.map((habitId, idx) => (
-                <HabitIndexItem habit={habits[habitId]} index={idx} key={habitId} />
-              ))}
-            </div>
+            <DragDropContext onDragEnd={this.dragEnd}>
+              
+              <div className="show-habits-header">
+                <h3>Your Habits</h3>
+              </div>
+
+              <Droppable droppableId="1">
+                {(provided) => 
+
+                  <div className="index-habit-container"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef} 
+                  >
+                    {user.habits.map((habitId, idx) => (
+                      <HabitIndexItem habit={habits[habitId]} index={idx} key={habitId} />
+                    ))}
+                    {provided.placeholder}
+                  </div>
+
+                }
+              </Droppable>
+
+            </DragDropContext>
           </div>
         );
     }
--- frontend/src/components/habit/habit_index_item.jsx
+++ frontend/src/components/habit/habit_index_item.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { Draggable } from 'react-beautiful-dnd'; 
 
 
 class HabitIndexItem extends React.Component {
@@ -9,14 +9,24 @@ class HabitIndexItem extends React.Component {
     const { habit } = this.props;
 
     return (
-      <div className="hit-container">
-        <Link to={`/habits/${habit._id}`}>
-          <p className="hit-title">
-            {habit.title}
-          </p>
-          <span>{habit.description}</span>
-        </Link>
-      </div>
+      <Draggable draggableId={habit._id} index={this.props.index}>
+        {(provided) => (
+
+          <div className="hit-container"
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef} 
+          >
+            <Link to={`/habits/${habit._id}`}>
+              <p className="hit-title">
+                {habit.title}
+              </p>
+              <span>{habit.description}</span>
+            </Link>
+          </div>
+
+        )}
+      </Draggable>
     );
   }
 
