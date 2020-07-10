b93d9c900593514116884bfea21ce6adf894a1f6
M3L6H <sisoac@hotmail.com>
2020-07-10 03:13:53 -0700

    Got dropdown working on desktop

--- frontend/src/components/ashe_nav_bar/nav_bar.jsx
+++ frontend/src/components/ashe_nav_bar/nav_bar.jsx
@@ -1,31 +1,56 @@
 import React from 'react';
 
 class NavBar extends React.Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      dropdownOpen: false
+    };
 
-    render(){
+    this._handleBodyClick = this._handleBodyClick.bind(this);
+    this._handleDropdownClick = this._handleDropdownClick.bind(this);
+  }
 
-        const landing = this.props.location.pathname === "/landing" ? "landing" : "" ;
-        const tasksLocation = this.props.location.pathname === "/tasks" ? "tasksLocation" : "" ;
-        const habitsLocation = this.props.location.pathname === "/habits" ? "habitsLocation" : "" ;
+  componentDidMount() {
+    document.body.addEventListener("click", this._handleBodyClick);
+  }
+  
+  _handleBodyClick() {
+    this.setState({ dropdownOpen: false });
+  }
 
-        return (
-          <div className="navbar-container">
-            <div className="navbar">
-              <div className={`link-home ${landing}`} onClick={() => this.props.history.push("/landing")}>Home</div>
-              <div className={`link-tasks ${tasksLocation}`} onClick={() => this.props.history.push("/tasks")}>Tasks</div>
-              <div className={`link-habits ${habitsLocation}`} onClick={() => this.props.history.push("/habits")}>Habits</div>
-              <div className="navbar-icons">
-                <i className="fa fa-bars"></i>
-                <div className="navbar-dropdown">
-                    <div onClick={() => this.props.history.push("/dashboard")}>Dashboard</div>
-                    <div onClick={() => this.props.history.push("/habit")}>Create Habits</div>
-                    <div onClick={this.props.logout}>LogOut</div>
-                </div>
-              </div>
+  _handleDropdownClick(e) {
+    e.stopPropagation();
+    this.setState({ dropdownOpen: !this.state.dropdownOpen });
+  }
+
+  render(){
+    const { dropdownOpen } = this.state;
+    
+    const landing = this.props.location.pathname === "/landing" ? "landing" : "" ;
+    const tasksLocation = this.props.location.pathname === "/tasks" ? "tasksLocation" : "" ;
+    const habitsLocation = this.props.location.pathname === "/habits" ? "habitsLocation" : "" ;
+
+    return (
+      <div className="navbar-container">
+        <div className="navbar">
+          <div className={`link-home ${landing}`} onClick={() => this.props.history.push("/landing")}>Home</div>
+          <div className={`link-tasks ${tasksLocation}`} onClick={() => this.props.history.push("/tasks")}>Tasks</div>
+          <div className={`link-habits ${habitsLocation}`} onClick={() => this.props.history.push("/habits")}>Habits</div>
+          <div className="navbar-icons">
+            <i className="fa fa-bars" onClick={ this._handleDropdownClick }></i>
+            <div className={`navbar-dropdown${ dropdownOpen ? " open" : "" }`}>
+                <div onClick={() => this.props.history.push("/landing")}>Home</div>
+                <div onClick={() => this.props.history.push("/dashboard")}>Dashboard</div>
+                <div onClick={() => this.props.history.push("/habit")}>Create Habits</div>
+                <div onClick={this.props.logout}>LogOut</div>
             </div>
           </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NavBar;
