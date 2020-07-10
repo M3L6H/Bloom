78f716ad2f5ee380b1cdb879a1c2846e86aa985d
M3L6H <sisoac@hotmail.com>
2020-07-10 09:44:36 -0700

    Created edit title form

---
+++ frontend/src/components/habit/edit_title_form.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+export default class EditTitleForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.textarea = React.createRef();
+
+    this.state = {
+      title: props.title
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    const range = document.createRange();
+    const selected = window.getSelection();
+    range.setStartBefore(this.textarea.current.childNodes[0]);
+    range.setEndAfter(this.textarea.current.childNodes[this.textarea.current.childNodes.length - 1]);
+    selected.removeAllRanges();
+    selected.addRange(range);
+    this.textarea.current.focus();
+  }
+
+  handleKeyDown(e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      this.handleSubmit(e);
+    }
+  }
+
+  //get updated title to update habit
+  handleSubmit(e) {
+    e.preventDefault();
+            
+    //habit title needs to be updated
+    const title = this.textarea.current.innerHTML
+      .replace(/<div>|<br>/g, "\n")
+      .replace(/<\/div>/g, "");
+    this.props.updateHabit({ ...this.props.habit, title });
+    this.props.hideEditForm(title);
+  }
+
+  //when edit title form is triggered, it renders edit form
+  render() {
+          
+      const { open } = this.props;
+      const { title } = this.state;
+      if (!title) return null; 
+
+      return (
+        <div className={`title-edit ${open}`}>
+          <div className="title-edit-container">
+            <pre
+              contentEditable={ true }
+              ref={ this.textarea }
+              type="text"
+              className="edit-title-input"
+              suppressContentEditableWarning={true}
+              onBlur={ this.handleSubmit }
+              onKeyDown={ this.handleKeyDown }
+            >{ title }</pre>
+          </div>
+        </div>
+      );
+  }
+};
+            
