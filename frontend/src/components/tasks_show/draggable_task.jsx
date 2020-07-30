cfb5ab677c9aa6f5ed18e6d86f391164382436ce
M3L6H <sisoac@hotmail.com>
2020-07-30 15:31:33 -0700

    Merged Ashe's styling with Ajay's task completion

--- frontend/src/components/tasks_show/draggable_task.jsx
+++ frontend/src/components/tasks_show/draggable_task.jsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import {Draggable} from 'react-beautiful-dnd'; 
-import TaskItemShow from "../landing/task_item_show";
+import TaskItemShow from '../landing/task_item_show';
 
 // Expects props task, habit, user, updatePetals(fcn), and updateTask(fcn)
-class DraggableTask extends React.Component {
-
+class DraggableTask extends TaskItemShow {
     render(){
-        const { task, habit , user, updatePetals, updateTask } = this.props;
+        const { task, habit } = this.props;
         if(!task || !habit) return null;
+        let completed;
+        let completedT;
+        const complete = this.state.complete ? "check-" : "";
+
+        if (task.numTimesDone === task.periodNum) {
+            completed = "COMPLETE";
+            completedT = "title-completed"
+        } else {
+            completed = "";
+        }
+
         return(
             <Draggable draggableId={task._id} index={this.props.index}>
                 { (provided) =>(
                     
-                    <div className="" 
+                    <div className={`taskshow-container ${ completed }`} 
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
                     ref={provided.innerRef} 
                     >
-                        <TaskItemShow task={task} habit={habit} updateTask={updateTask} updatePetals={updatePetals} user={user}/>
+                      { !completed && 
+                        <div className="task-complete"><i className={`far fa-${complete}square`} onClick={this.completeTask}></i></div>
+                      }
+                      <div className="taskshow-completion">{completed}</div>
+                      <div className={`taskshow-title ${completedT}`}>{task.title} <span>- {habit.title}</span></div>
+                      <div className="taskshow-detail">
+                          <div className="frequency-and-petals">
+                              <div className="task-item-frequency">
+                                  Goal: {task.numTimesDone}/{task.periodNum} per {task.periodUnit}
+                              </div>
+                              <div className="task-item-petals">
+                                  Reward: { `${ task.numPetals } Petal${ task.numPetals > 1 ? "s" : "" }` }
+                              </div>
+                          </div>
+                      </div>
                     </div>
                 )}
             </Draggable>
