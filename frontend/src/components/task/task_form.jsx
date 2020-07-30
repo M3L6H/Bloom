8e651b9110b518022e721f86e34aae67d21b9fa0
M3L6H <sisoac@hotmail.com>
2020-07-30 16:18:08 -0700

    Refactoring task form

--- frontend/src/components/task/task_form.jsx
+++ frontend/src/components/task/task_form.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Form, Dropdown } from 'semantic-ui-react';
 
 class TaskForm extends React.Component {
 
@@ -11,8 +12,15 @@ class TaskForm extends React.Component {
     this.state.numPetals = this.state.numPetals > 0 ? this.state.numPetals : 1;
     this.state.periodUnit = this.state.periodUnit.length > 0 ? this.state.periodUnit : "day";
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
 
+    this.placeholders = [
+      "Brush teeth",
+      "Drink water",
+      "Cook lunch",
+      "Go for a run",
+      "Walk the dog"
+    ];
+  }
 
   handleSubmit(e) {
     e.preventDefault();
@@ -64,44 +72,61 @@ class TaskForm extends React.Component {
       </select>
     );
 
-    const renderTitle = 
+    const formTitle = 
       formType === "editTask" ? (
         "Edit Task") : ("Create Task")
     
     return (
       <div className="task-modal-background">
-        <form className="task-form">
-          <div className="task-form-top">{renderTitle}</div>
-          <textarea
-            className="task-form-title"
-            type="textarea"
-            value={this.state.title}
-            placeholder="Add your task"
-            onChange={this.update("title")}
-          />
-          <div className="task-form-input">
-            <div className="task-form-num-input">
+        <Form className="task-form">
+          <div className="task-form-top">{ formTitle }</div>
+          <Form.Field>
+            <label>Title</label>
+            <input 
+              placeholder={ this.placeholders[Math.floor(Math.random() * this.placeholders.length)] }
+              value={ this.state.title }
+              onChange={ this.update("title") }
+            />
+          </Form.Field>
+          <Form.Group widths="equal">
+            <Form.Field>
               <input
                 type="number"
                 min="1"
-                value={this.state.periodNum}
-                onChange={this.update("periodNum")}
-              />{" "}
-            times per {freqDropDown}
-            </div>
-            <div className="task-form-petal-cnt">
-              Get{" "}
-              <input
-                type="number"
-                min="1"
-                value={this.state.numPetals}
-                onChange={this.update("numPetals")}
-              />{" "}
-              petals per completion
-            </div>
-            <div className="submit-task-btn">{submitButton}</div>
-          </div>
-        </form>
+                value={ this.state.periodNum }
+                onChange={ this.update("periodNum") }
+              />
+            </Form.Field>
+            <Form.Field>
+              <Dropdown
+                placeholder="Select Period"
+                selection
+                onChange={ this.update("periodUnit") }
+                value={ this.state.periodUnit }
+                options={ [
+                  { key: "Day", text: "per Day", value: "day" },
+                  { key: "Week", text: "per Week", value: "week" },
+                  { key: "Month", text: "per Month", value: "month" },
+                  { key: "Year", text: "per Year", value: "year" },
+                ] }
+              />
+            </Form.Field>
+          </Form.Group>
+          <Form.Field>
+            <label>Reward</label>
+            <Dropdown
+              placeholder="Select Reward"
+              selection
+              onChange={ this.update("numPetals") }
+              value={ this.state.numPetals }
+              options={ [
+                { key: "Small", text: "Small (1 petal)", value: 1 },
+                { key: "Medium", text: "Medium (5 petals)", value: 5 },
+                { key: "Large", text: "Large (10 petals)", value: 10 },
+              ] }
+            />
+          </Form.Field>
+        </Form>
       </div>
     );
   }
