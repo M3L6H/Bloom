1dcfb72b2c2d894a08da33c475af75ca9738c688
Ashe <ashe@nyu.edu>
2020-07-30 15:05:45 -0700

    completed tasks change looks in tasks list

--- frontend/src/components/tasks_show/task_show.jsx
+++ frontend/src/components/tasks_show/task_show.jsx
@@ -5,16 +5,27 @@ class TaskShow extends React.Component {
     render(){
         const { task, habit } = this.props;
         if(!task || !habit) return null;
+        let completed;
+        let completedT;
+
+        if (task.numTimesDone === task.periodNum) {
+            completed = "COMPLETE";
+            completedT = "title-completed"
+        } else {
+            completed = "";
+        }
+
         return(
             <Draggable draggableId={task._id} index={this.props.index}>
                 { (provided) =>(
                     
-                    <div className="taskshow-container" 
+                    <div className={`taskshow-container ${completed}`} 
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
                     ref={provided.innerRef} 
                     >
-                        <div className="taskshow-title">{task.title} <span>- {habit.title}</span></div>
+                        <div className="taskshow-completion">{completed}</div>
+                        <div className={`taskshow-title ${completedT}`}>{task.title} <span>- {habit.title}</span></div>
                         <div className="taskshow-detail">
                             <div className="frequency-and-petals">
                                 <div className="task-item-frequency">
