836e4a4ae0b2bfb21745f0273c96b859ee13fc2b
M3L6H <sisoac@hotmail.com>
2020-07-31 10:42:29 -0700

    Clicking on task in DnD list now opens edit task modal

--- frontend/src/components/tasks_show/tasks_show_container.js
+++ frontend/src/components/tasks_show/tasks_show_container.js
@@ -2,7 +2,9 @@ import { connect } from 'react-redux';
 import TasksShow from './tasks_show';
 import { fetchHabits } from '../../actions/habits_actions';
 import { updateTask, deleteTask } from '../../actions/tasks_actions';
-import { fetchUser, updatePetals, updateDailyTaskList, sortDailyTaskList} from "../../actions/users_actions"
+import { fetchUser, updatePetals, updateDailyTaskList, sortDailyTaskList} from "../../actions/users_actions";
+import { openModal } from '../../actions/modal_actions';
+
 const mSTP = state => ({
     habits: state.entities.habits,
     tasks: state.entities.tasks,
@@ -17,6 +19,7 @@ const mDTP = dispatch => ({
     fetchUser: ()=> dispatch(fetchUser()),
     updateDailyTaskList: (newTaskList) => dispatch(updateDailyTaskList(newTaskList)),
     sortDailyTaskList: ()=> dispatch(sortDailyTaskList()),
+    openModal: (type, object) => dispatch(openModal(type, object))
 });
 
 export default connect(mSTP, mDTP)(TasksShow);
